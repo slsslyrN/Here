fa603affa0f732445ca15b8199987e4149912fa8
BryanRTrabajo <br.rivas@duocuc.cl>
2025-01-23 22:16:26 -0300

    Se agrega login funcional

---
+++ Here/src/app/login/login.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  imports: [IonicModule]
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
